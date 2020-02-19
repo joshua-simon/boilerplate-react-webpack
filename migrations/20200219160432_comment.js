b2adac69746d65560ff896b7d7aa2064bfada42c
Student <student@eda-bc-02.localdomain>
2020-02-19 16:35:14 +1300

    final

--- migrations/20200219160432_comment.js
+++ migrations/20200219160432_comment.js
@@ -1,8 +1,10 @@
 
 exports.up = function(knex) {
-  
+    knex.schema.createTable('comment', (table) => {
+        table.increments('id').primary()
+        table.string('comment')
 };
 
 exports.down = function(knex) {
-  
+    return knex.schema.dropTable('comment')
 };
